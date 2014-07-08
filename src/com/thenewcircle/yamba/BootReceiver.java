8f0bb3f9fab51d5a8940c1703632612aedaa829c
Ron Munitz <ron@android-x86.org>
2014-07-08 07:31:13 -0400

    Added Broadcast Receiver to handle BOOT_COMPLETED events

---
+++ src/com/thenewcircle/yamba/BootReceiver.java
@@ -0,0 +1,16 @@
+package com.thenewcircle.yamba;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootReceiver extends BroadcastReceiver {
+	private static final String TAG = BootReceiver.class.getSimpleName();
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Log.d(TAG,"onReceived: " + intent.toString());
+	}
+
+}
