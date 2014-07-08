a248528d8a6813adb1ac750455892102e1b8c4f7
Ron Munitz <ron@android-x86.org>
2014-07-08 08:50:57 -0400

    Added repeating PendingIntent using AlarmManager

--- src/com/thenewcircle/yamba/BootReceiver.java
+++ src/com/thenewcircle/yamba/BootReceiver.java
@@ -1,5 +1,7 @@
 package com.thenewcircle.yamba;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -7,10 +9,22 @@ import android.util.Log;
 
 public class BootReceiver extends BroadcastReceiver {
 	private static final String TAG = BootReceiver.class.getSimpleName();
+	private static final long DEFAULT_INTERVAL = 30000; // 30 secs just for the
+														// example
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		Log.d(TAG,"onReceived: " + intent.toString());
+
+		PendingIntent operation = PendingIntent.getService(context, -1,
+				new Intent(context, RefreshService.class),
+				PendingIntent.FLAG_UPDATE_CURRENT);
+
+		AlarmManager alarmManager = (AlarmManager) context
+				.getSystemService(Context.ALARM_SERVICE);
+		alarmManager.setInexactRepeating(AlarmManager.RTC,
+				System.currentTimeMillis(), DEFAULT_INTERVAL, operation);
+
+		Log.d(TAG, "onReceived");
 	}
 
 }
